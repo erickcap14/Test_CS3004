039a9f31569c760ac5f9fd4e9f8fda76862b1ee4
erickcap15@gmail.com <erickcap15@gmail.com>
2023-11-17 19:52:46 +0000

    Edited MilitaryMaps.Map and MilitaryMaps

--- client_code/MilitaryMaps/Map/__init__.py
+++ client_code/MilitaryMaps/Map/__init__.py
@@ -7,3 +7,7 @@ class Map(MapTemplate):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+
+  def card_1_show(self, **event_args):
+    """This method is called when the column panel is shown on the screen"""
+    pass
--- client_code/MilitaryMaps/__init__.py
+++ client_code/MilitaryMaps/__init__.py
@@ -7,3 +7,7 @@ class MilitaryMaps(MilitaryMapsTemplate):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+
+  def NewButton_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    open_form('MilitaryMaps.Map')
